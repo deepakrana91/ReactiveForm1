5d295536c96bb59115740eacca464a72c224bdd9
= <=>
2017-09-22 15:11:35 +0530

    Simple Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { FormdataService } from './formdata.service';
+import { FormService } from './form.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [FormService,FormdataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/form.service.ts
@@ -0,0 +1,10 @@
+
+
+export class FormService {
+
+  constructor() { }
+
+  title : any [] = [{id:1,value:'Mr'},{id:2,value:'Mrs'}];
+  
+
+}
